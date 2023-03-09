c8549b85eb832569fd1e32bedb655643dfbfd11c
Tal <talrasha007@gmail.com>
2023-03-09 12:27:30 +0800

    head.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="head">
+    <img src="./assets/bot.svg" class="logo" />
+    <span>狗屁通</span>
+    <span class="ph">123</span>
+    <span class="cool">Cool</span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.head {
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+  font-weight: bold;
+  .logo {
+    height: 48px;
+    will-change: filter;
+    transition: filter 300ms;
+    margin-right: 10px;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .ph {
+    color: #000;
+    background-color: #ff9900;
+    border-radius: 3px;
+    margin: 5px;
+    padding: 0 5px;
+  }
+  .cool{
+    background: linear-gradient(to right, rgba(56,189,248,var(--un-from-opacity, 1)), rgba(5,150,105,var(--un-to-opacity, 1)));
+    -webkit-background-clip: text;
+    color: transparent;
+  }
 }
 </style>
