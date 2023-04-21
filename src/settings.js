257a59104c7f3a24432ee5bed1c3cecbed7e0109
Tal <talrasha007@gmail.com>
2023-04-21 13:10:28 +0800

    update settings.

--- src/settings.js
+++ src/settings.js
@@ -5,10 +5,10 @@ const roles = [{
   name: '-'
 }, {
   name: '作图',
-  message: '我的话是一幅图像的大致内容，请补充细节，生成AI绘画的prompt',
+  message: '我的话是一幅图像的大致内容，请补充细节，生成描述（注意：只回复描述）',
   draw: true
 }, {
-  name: '作图-英文',
+  name: '作图-英文输出',
   message: '我的话是一幅图像的大致内容，请补充细节，用英文描述一幅AI绘画的prompt',
   draw: true,
   english: true
