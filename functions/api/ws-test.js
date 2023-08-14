3f7b960c29f16584bc980bb607e75b6bcd50a209
talrasha007 <talrasha007@gmail.om>
2023-08-14 18:21:18 +0800

    ws open.

--- functions/api/ws-test.js
+++ functions/api/ws-test.js
@@ -4,20 +4,21 @@ export function onRequest({ env }) {
     const { readable, writable } = new TransformStream()
     const writer = writable.getWriter()
 
+    const tm = setTimeout(reject, 5000)
     const ws = new WebSocket('wss://ws.gpt123.cool/?encoding=json&v=9')
     ws.addEventListener('open', () => {
-      console.log('ws open')
       clearTimeout(tm)
-      resolve(new Response(readable, { status: 200, statusText: 'OK', headers: { 'Content-Type': 'text/html' } }))
+      console.log('ws open')
+
       ws.send(`{"op":2,"d":{"token":"${env.MJ_TOKEN}","capabilities":8189,"properties":{"os":"Mac OS X","browser":"Chrome","device":""},"compress":false}}`)
 
       setTimeout(() => writer.close(), 3000)
+      resolve(new Response(readable, { status: 200, statusText: 'OK', headers: { 'Content-Type': 'text/html' } }))
     })
 
     ws.addEventListener('message', (event) => {
       writer.write(event.data + '\n')
     })
     
-    const tm = setTimeout(reject, 5000)
   })
 }
