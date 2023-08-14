f067cf4aaa0733eaf254306298162c2145461215
talrasha007 <talrasha007@gmail.om>
2023-08-14 21:08:42 +0800

    chat page function.

---
+++ functions/api/chat.js
@@ -0,0 +1,19 @@
+
+export async function onRequest({ request, env }) {
+  if (request.method !== 'POST') return new Response('Hmm~~~?', { status: 404, statusText: 'Not Found.' })
+
+  const chat = await request.json()
+  const { body, headers, status, statusText } = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo', temperature: 0.6,
+      ...chat
+    }),
+    headers: {
+      'Authorization': `Bearer ${env.GPT_TOKEN}`,
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return new Response(body, { status, statusText, headers })
+}
