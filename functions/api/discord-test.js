0b7c7c6b6e3901830a24eaa97b0687bfa3a0252a
talrasha007 <talrasha007@gmail.om>
2023-08-14 18:41:46 +0800

    discord.gg test.

---
+++ functions/api/discord-test.js
@@ -0,0 +1,26 @@
+
+export function onRequest({ env }) {
+  return new Promise((resolve, reject) => {
+    const { readable, writable } = new TransformStream()
+    const writer = writable.getWriter()
+    const encoder = new TextEncoder()
+
+    const tm = setTimeout(reject, 5000)
+    const ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=9')
+    ws.addEventListener('open', () => {
+      clearTimeout(tm)
+      console.log('ws open')
+      writer.write(encoder.encode('ws connected...\n'))
+
+      ws.send(`{"op":2,"d":{"token":"${env.MJ_TOKEN}","capabilities":8189,"properties":{"os":"Mac OS X","browser":"Chrome","device":""},"compress":false}}`)
+
+      setTimeout(() => writer.close(), 15000)
+      resolve(new Response(readable, { status: 200, statusText: 'OK', headers: { 'Content-Type': 'text/html' } }))
+    })
+
+    ws.addEventListener('message', (event) => {
+      console.log('message:', event)
+      writer.write(encoder.encode(event.data + '\n'))
+    })
+  })
+}
