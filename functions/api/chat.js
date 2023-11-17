7b5b61c26649a166941499dc3866eb9d4ac379e6
Tal Rasha <talrasha007@gmail.com>
2023-11-17 11:32:57 +0800

    use proxy

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,10 +1,12 @@
+// const url = 'https://api.openai.com/v1/chat/completions'
+const url = 'https://proxy.gpt123.cool/v1/chat/completions'
 
 export async function onRequest({ request, env }) {
   if (request.method !== 'POST') return new Response('Hmm~~~?', { status: 404, statusText: 'Not Found.' })
   const authorization = request.headers.get('Authorization')
 
   const chat = await request.json()
-  const { body, headers, status, statusText } = await fetch('https://api.openai.com/v1/chat/completions', {
+  const { body, headers, status, statusText } = await fetch(url, {
     method: 'POST',
     body: JSON.stringify({
       temperature: 0.6,
