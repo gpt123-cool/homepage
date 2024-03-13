aeeabdf51fc5d9be54476a0dad9d9e921bde4299
talrasha007 <talrasha007@gmail.om>
2024-03-14 07:04:39 +0800

    in english

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,5 +1,5 @@
 export const MIDJOURNEY_EXPLANATION = 
-`Your goal is to create Midjourney prompts, in the form of [some prompt].
+`Your goal is to create Midjourney prompts, in the form of [some prompt in English].
 Midjourney is an AI program that generates images from natural language prompts. 
 Weights: you can give weights to part of the prompt, and it can be really important to stress some concepts. Most of the time they're not needed.
 To use them, add [SUBJECT]::WEIGHT to the prompt, e.g., [A house]::2 [that looks like a cat]::3 [in the style of X]::3.
